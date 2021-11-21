346c6e66b46d646aa3c1bef6fea65854fd39451a
vaisajuh <juha.vaisanen@gmail.com>
2021-11-21 15:11:39 +0200

    Update handle_database

--- src/handle_database.py
+++ src/handle_database.py
@@ -13,10 +13,47 @@ class Handle_database:
     def initialize_database(self):
         try:
             for i in self.tables:
-                self.database.execute(','.join(i))
+                self.database.execute(''.join(i))
         except:
             ""  
+    
+
+    def clear_database(self):
+        self.database.execute('DELETE FROM Users')
+        self.database.execute('DELETE FROM Contacts')
+        print('-------')
+    
+
+    def get_users(self):
+        get_users = self.database.execute('SELECT name FROM Users').fetchall()
+        for i in get_users:
+            print(', '.join(i))
+    
+
+    def get_contacts(self):
+        get_contacts = self.database.execute('SELECT name, address, email, phonenumber FROM Contacts').fetchall()
+        for i in get_contacts:
+            print(', '.join(i))
+    
+
+    def insert_user(self, name: str):
+        self.database.execute('INSERT INTO Users (name) VALUES (?)', [name])
+
+
+    def insert_contact(self, name: str, address: str, email: str, phonenumber: str):
+        self.database.execute('INSERT INTO Contacts (name, address, email, phonenumber)'\
+            'VALUES (?,?,?,?)', [name, address, email, phonenumber])
 
 if __name__ == "__main__":
-    jees = Handle_database()
-    jees.initialize_database()
+    test = Handle_database()
+    test.initialize_database()
+    test.insert_user("user")
+    test.insert_contact("name", "street", "x@x.com", "0123456789")
+    test.get_contacts()
+    test.get_users()
+    test.clear_database()
+    test.insert_user("user")
+    test.insert_contact("name", "street", "x@x.com", "0123456789")
+    test.get_contacts()
+    test.get_users()
+
