94b04047208e5c4cbe9441e4366144754fe49ec9
vaisajuh <juha.vaisanen@gmail.com>
2021-11-30 23:35:19 +0200

    Update users_test

--- src/tests/handle_users_test.py
+++ src/tests/handle_users_test.py
@@ -6,19 +6,22 @@ class TestHandleUsers(unittest.TestCase):
     def setUp(self):
         self.test = HandleDatabase()
         self.test.clear_database()
+        self.user = self.test.users.insert_user("Michelin", "1234")
 
 
     def test_insert_user(self):
-        self.test.users.insert_user("Michelin", "1234")
         get = self.test.users.get_users()
         value = get[0]
         self.assertEqual(value, "michelin, 1234")
 
 
     def test_validate_password(self):
-        user_creation = self.test.users.validate_password("Michelin", "1234")
-        self.assertEqual(user_creation, 1)
         invalid_pass = self.test.users.validate_password("Michelin", "1235")
         self.assertEqual(invalid_pass, False)
         correct_pass = self.test.users.validate_password("Michelin", "1234")
         self.assertEqual(correct_pass, 1)
+    
+    
+    def test_validate_count_0(self):
+        user_creation = self.test.users.validate_password("Monica", "1234")
+        self.assertEqual(user_creation, 2)
