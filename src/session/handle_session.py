5c451d72ab0b55b86d93522fae3c01629dd8a10f
Juha Väisänen <81490976+vaisajuh@users.noreply.github.com>
2021-12-11 23:38:37 +0200

    Update handle_session.py

--- src/session/handle_session.py
+++ src/session/handle_session.py
@@ -1,11 +1,21 @@
 class HandleSession:
+    """Luokka, joka vastaa kirjautuneista käyttäjistä
+    session: Kirjautuneet käyttäjät tallennetaan listaan
+    """
 
     def __init__(self):
+        """Luokan konstruktori, joka luo listan"""
+        
         self.session = []
 
     def add_session(self, user_id):
+        """Lisää kirjatuneen käyttäjän id-numeron listaan"""
+        
         self.session.append(user_id)
 
     def get_session(self):
+        """Palauttaa viimeksi kirjautuneen käyttäjän id-numeron 
+        listasta"""
+        
         user_id = self.session[-1]
         return user_id
