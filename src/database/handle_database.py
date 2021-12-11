a6bcc7d3b069aab8dd1138c69669fa7be33aff2a
Juha Väisänen <81490976+vaisajuh@users.noreply.github.com>
2021-12-11 23:13:39 +0200

    Update handle_database.py

--- src/database/handle_database.py
+++ src/database/handle_database.py
@@ -6,8 +6,18 @@ from storage.handle_users import HandleUsers
 
 
 class HandleDatabase:
+    """Luokka, joka vastaa tietokantayhteydestä, sen alustamisesta ja
+    tarvittaessa kaikkien tietojen poistamisesta
+    
+    Attributes:
+    database: Yhteys tietokantaan
+    tables: Tietokantaan lisättävät taulut
+    users: Luokan users logiikka, joka saa parametrina tietokantayhteyden
+    contacts: Luokan contacts logiikka, joka saa parametrina tietokantayhteyden
+    """
 
     def __init__(self):
+        """Luokan konstruktori, joka alustaa muuttujat"""
         self.database = sqlite3.connect(CONTACTS_DATABASE_PATH)
         self.tables = csv_reader.get_database_tables()
         self.users = HandleUsers(self.database)
@@ -15,10 +25,15 @@ class HandleDatabase:
         self.initialize_database()
 
     def initialize_database(self):
+        """Lisää tietokantaan atribuutissa tablessa olevat
+        tietokantataulut"""
+        
         for i in self.tables:
             self.database.execute(''.join(i))
 
     def clear_database(self):
+        """Poistaa kaiken tiedon tietokannan tauluista"""
+        
         self.database.execute('DELETE FROM Users')
         self.database.execute('DELETE FROM Contacts')
         self.database.commit()
