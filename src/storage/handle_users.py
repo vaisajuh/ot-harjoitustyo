ec1e8276065f3838915c1562c708fef05ed2e6b3
Juha Väisänen <81490976+vaisajuh@users.noreply.github.com>
2021-12-12 00:25:13 +0200

    Update handle_users.py

--- src/storage/handle_users.py
+++ src/storage/handle_users.py
@@ -1,15 +1,24 @@
 
 class HandleUsers:
+    """Luokka hallinnoi tietokantaan kirjautuneita käyttäjiä 
+    Attributes:
+    database: Tietokantayhteys
+    """
 
     def __init__(self, database):
+        """Luokan konstruktori"""
+
         self.database = database
 
     def get_user(self, name: str):
+        """Palauttaa parametrina annetun käyttäjän id-numeron """
         get_user = self.database.execute(
             'SELECT id, name, password FROM Users WHERE name like ?', [name]).fetchall()
         return list(get_user[0])
 
     def insert_user(self, name: str, password: str):
+        """Luo uuden käyttäjän tietokantaan"""
+
         name = name.lower()
         password = password.lower()
         self.database.execute(
@@ -17,6 +26,10 @@ class HandleUsers:
         self.database.commit()
 
     def validate_password(self, name: str, password: str):
+        """Hakee käyttäjää tietokannasta ja jos löytyy, niin tarkastaa 
+        salasanan oikeellisuuden. Jos käyttäjää ei löydy tietokannasta, niin 
+        se lisätään sinne"""
+
         count = self.database.execute(
             'SELECT COUNT(*) FROM Users WHERE name LIKE ?', [name]).fetchone()[0]
         get_id = ""
