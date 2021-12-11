98b5afb93bbfb77207d34f0f19f4855b2016888e
vaisajuh <juha.vaisanen@gmail.com>
2021-12-11 23:50:11 +0200

    Update handle_session

--- src/session/handle_session.py
+++ src/session/handle_session.py
@@ -16,13 +16,8 @@ class HandleSession:
         self.session.append(user_id)
 
     def get_session(self):
-<<<<<<< HEAD
         """Palauttaa viimeksi kirjautuneen käyttäjän id-numeron 
         listasta"""
-
-=======
-        """Palauttaa viimeksi kirjautuneen käyttäjän id-numeron listasta"""
         
->>>>>>> 7ddaa855cb7393d5f039f909f178a4568ae5b9ce
         user_id = self.session[-1]
         return user_id
