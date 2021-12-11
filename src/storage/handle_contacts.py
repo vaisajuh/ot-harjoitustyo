c5dc88197419bd9e35a520579bef79cbbff78fe4
Juha Väisänen <81490976+vaisajuh@users.noreply.github.com>
2021-12-12 00:09:29 +0200

    Update handle_contacts.py

--- src/storage/handle_contacts.py
+++ src/storage/handle_contacts.py
@@ -1,15 +1,26 @@
 class HandleContacts:
-
+    """Luokan tehtävänä hoitaa yhteystietojen hallinnointi
+    
+   Attributes:
+   database: Yhteys tietokantaan
+    
+    """
     def __init__(self, database):
+        """Luokan konstruktori"""
+        
         self.database = database
 
     def get_contacts(self, user_id):
+        """Palauttaa kirjautuneena olevan käyttäjän kontaktit"""
+        
         get_contacts = self.database.execute('SELECT c.name, c.address, c.email, c.phonenumber,'
         ' c.id FROM Users u JOIN Contacts c on u.id = c.user_id WHERE u.id = ?',
         [user_id]).fetchall()
         return get_contacts
 
     def insert_contact(self,  user_id: int, name: str, address: str, email: str, phonenumber: str):
+        """Kirjautunut käyttäjä voi lisätä tällä yhteystietoja tietokantaan"""
+        
         name = name.lower()
         address = address.lower()
         email = email.lower()
@@ -18,5 +29,7 @@ class HandleContacts:
         self.database.commit()
 
     def delete_contact(self, row_id: int):
+        """Toiminto yhteystiedon poistamiseen"""
+        
         self.database.execute('DELETE FROM Contacts WHERE id = ?', [row_id])
         self.database.commit()
