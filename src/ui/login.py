0042810c34ee806be903b2136ca94c5c46b8deeb
vaisajuh <juha.vaisanen@gmail.com>
2021-12-12 15:04:23 +0200

    Update login

--- src/ui/login.py
+++ src/ui/login.py
@@ -8,17 +8,17 @@ class Login:
         self.functionality = functionality
         self.db = db
         self.session = session
-        self.login = None
-        self.username_entry = None
-        self.password_entry = None
+        self._login = None
+        self._username_entry = None
+        self._password_entry = None
 
     def destroy(self):
-        self.login.destroy()
+        self._login.destroy()
 
-    def validate_login(self):
-        if len(self.username_entry.get()) in range(4, 20) and\
-                len(self.password_entry.get()) in range(4, 20):
-            self.validate_password()
+    def _validate_login(self):
+        if len(self._username_entry.get()) in range(4, 20) and\
+                len(self._password_entry.get()) in range(4, 20):
+            self._validate_password()
 
         else:
             showwarning(
@@ -27,9 +27,9 @@ class Login:
                 "kahdenkymmenen merkin väliltä"
             )
 
-    def validate_password(self):
+    def _validate_password(self):
         validate = self.db.users.validate_password(
-            self.username_entry.get(), self.password_entry.get())
+            self._username_entry.get(), self._password_entry.get())
         if validate == False:
             showwarning(
                 title="Tiedoksi",
@@ -41,21 +41,21 @@ class Login:
 
     def start_login(self):
 
-        self.login = ttk.Frame(master=self.root)
-        self.login.pack(padx=10, pady=10, fill='x', expand=True)
+        self._login = ttk.Frame(master=self.root)
+        self._login.pack(padx=10, pady=10, fill='x', expand=True)
 
-        self.username_label = ttk.Label(master=self.login, text="Käyttäjänimi")
-        self.username_label.pack(fill='x', expand=True)
+        self._username_label = ttk.Label(master=self._login, text="Käyttäjänimi")
+        self._username_label.pack(fill='x', expand=True)
 
-        self.username_entry = ttk.Entry(master=self.login)
-        self.username_entry.pack(fill='x', expand=True)
+        self._username_entry = ttk.Entry(master=self._login)
+        self._username_entry.pack(fill='x', expand=True)
 
-        password_label = ttk.Label(master=self.login, text="Salasana")
-        password_label.pack(fill='x', expand=True)
+        self._password_label = ttk.Label(master=self._login, text="Salasana")
+        self._password_label.pack(fill='x', expand=True)
 
-        self.password_entry = ttk.Entry(master=self.login, show="*")
-        self.password_entry.pack(fill='x', expand=True)
+        self._password_entry = ttk.Entry(master=self._login, show="*")
+        self._password_entry.pack(fill='x', expand=True)
 
         login_button = ttk.Button(
-            master=self.login, text="Kirjaudu", command=self.validate_login)
+            master=self._login, text="Kirjaudu", command=self._validate_login)
         login_button.pack(fill='x', expand=True, pady=10)
