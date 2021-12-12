1cfdc7e318669e2d4a94459d0d5dfd47b200633f
vaisajuh <juha.vaisanen@gmail.com>
2021-12-12 14:56:14 +0200

    Update show_contacts

--- src/ui/show_contacts.py
+++ src/ui/show_contacts.py
@@ -8,24 +8,24 @@ class ShowContacts:
         self.functionality = functionality
         self.db = db
         self.session = session
-        self.show_contacts = None
-        self.tree = None
+        self._show_contacts = None
+        self._tree = None
 
     def destroy(self):
-        self.show_contacts.destroy()
+        self._show_contacts.destroy()
 
-    def insert_contacts(self):
+    def _insert_contacts(self):
         get_contacts = self.db.contacts.get_contacts(
             self.session.get_session())
         for i in get_contacts:
             ind = i[4]
-            self.tree.insert('', 'end', text=ind,
+            self._tree.insert('', 'end', text=ind,
                              values=(i[0], i[1], i[2], i[3]))
 
-    def remove_button(self):
+    def _remove_button(self):
         try:
-            row_value = self.tree.focus()
-            row_id = int(self.tree.item(row_value)["text"])
+            row_value = self._tree.focus()
+            row_id = int(self._tree.item(row_value)["text"])
             self.db.contacts.delete_contact(row_id)
             self.destroy()
             self.start_show_contacts()
@@ -34,23 +34,23 @@ class ShowContacts:
 
     def start_show_contacts(self):
         self.root.geometry('')
-        self.show_contacts = ttk.Frame(master=self.root)
-        self.show_contacts.grid()
-        self.tree = ttk.Treeview(master=self.show_contacts, column=(
+        self._show_contacts = ttk.Frame(master=self.root)
+        self._show_contacts.grid()
+        self._tree = ttk.Treeview(master=self._show_contacts, column=(
             "Nimi", "Osoite", "Email", "Puhelinnumero"), show='headings')
-        self.tree.column("# 1", anchor=CENTER)
-        self.tree.heading("# 1", text="Nimi")
-        self.tree.column("# 2", anchor=CENTER)
-        self.tree.heading("# 2", text="Osoite")
-        self.tree.column("# 3", anchor=CENTER)
-        self.tree.heading("# 3", text="Email")
-        self.tree.column("# 4", anchor=CENTER)
-        self.tree.heading("# 4", text="Puhelinnumero")
-        self.insert_contacts()
-        self.tree.grid()
+        self._tree.column("# 1", anchor=CENTER)
+        self._tree.heading("# 1", text="Nimi")
+        self._tree.column("# 2", anchor=CENTER)
+        self._tree.heading("# 2", text="Osoite")
+        self._tree.column("# 3", anchor=CENTER)
+        self._tree.heading("# 3", text="Email")
+        self._tree.column("# 4", anchor=CENTER)
+        self._tree.heading("# 4", text="Puhelinnumero")
+        self._insert_contacts()
+        self._tree.grid()
         remove_button = ttk.Button(
-            master=self.show_contacts, text="Poista", command=self.remove_button)
+            master=self._show_contacts, text="Poista", command=self._remove_button)
         remove_button.grid(column=1)
         reverse_button = ttk.Button(
-            master=self.show_contacts, text="Palaa edelliseen näkymään", command=self.functionality)
+            master=self._show_contacts, text="Palaa edelliseen näkymään", command=self.functionality)
         reverse_button.grid()
